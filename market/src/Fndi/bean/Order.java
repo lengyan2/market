c212dc70488e75253417a479a99a46cec59ef984
lengyan2 <1304077301@qq.com>
2021-06-30 15:53:15 +0800

    bean

---
+++ market/src/Fndi/bean/Order.java
@@ -0,0 +1,88 @@
+package Fndi.bean;
+
+import Fndi.utils.OrderStatusType;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order implements Serializable {
+    private int orderId; //订单id
+    private List<OrderItem> orderItemList = new ArrayList<>(); //订单项集合
+    private String createDate; // 创建时间
+    private float sum ; // 总金额
+    private OrderStatusType status = OrderStatusType.UNPAID; // 状态
+    private int userId;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", orderItemList=" + orderItemList +
+                ", createDate='" + createDate + '\'' +
+                ", sum=" + sum +
+                ", status=" + status +
+                ", userId=" + userId +
+                '}';
+    }
+
+    public Order(int orderId, List<OrderItem> orderItemList, String createDate, float sum, OrderStatusType status, int userId) {
+        this.orderId = orderId;
+        this.orderItemList = orderItemList;
+        this.createDate = createDate;
+        this.sum = sum;
+        this.status = status;
+        this.userId = userId;
+    }
+
+    public Order() {
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public List<OrderItem> getOrderItemList() {
+        return orderItemList;
+    }
+
+    public void setOrderItemList(List<OrderItem> orderItemList) {
+        this.orderItemList = orderItemList;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public float getSum() {
+        return sum;
+    }
+
+    public void setSum(float sum) {
+        this.sum = sum;
+    }
+
+    public OrderStatusType getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatusType status) {
+        this.status = status;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
---
+++ market/src/Fndi/utils/OrderStatusType.java
@@ -0,0 +1,6 @@
+package Fndi.utils;
+
+public enum OrderStatusType {
+    UNPAID, // 未付款
+    PAID  // 已付款
+}
